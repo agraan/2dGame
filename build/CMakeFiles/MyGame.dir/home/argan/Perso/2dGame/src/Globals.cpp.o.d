cb8a75dc228ca8dee07be6776083b00a1ea8e5be
Argan <argamin45@gmail.com>
2025-10-22 01:37:31 +0200

    Cleaned

--- build/CMakeFiles/MyGame.dir/home/argan/Perso/2dGame/src/Globals.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MyGame.dir/home/argan/Perso/2dGame/src/Globals.cpp.o: \
- /home/argan/Perso/2dGame/src/Globals.cpp /usr/include/stdc-predef.h \
- /home/argan/Perso/2dGame/cmake/../include/Globals.hpp
